92342ee24922284d4fbf5217d20411381b521ae4
xjn <98167264+surge7197@users.noreply.github.com>
2023-01-03 10:25:40 +0800

    Delete edu directory

--- edu/dao/DepartDao.java
+++
@@ -1,7 +0,0 @@
-package edu.dao;
-
-public interface DepartDao {
-    String getdnames(String dno); //根据学科号获取学科名
-    int getask(String dno); //根据学科号获取是否开启助教选课
-    void updatesak(int dask,String dno);  //根据学科编号修改助教选课
-}
